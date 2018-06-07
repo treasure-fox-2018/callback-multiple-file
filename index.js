acfd31d7ed5f6dbec3fc2ee2234dda7b4ea6db67
Windi <windi@hacktiv8.com>
2018-06-07 13:52:19 +0700

    Template

---
+++ index.js
@@ -0,0 +1,9 @@
+const fs = require('fs');
+var sleep = require('sleep');
+
+function match_data(parent_file, children_file) {
+  // your code here...
+}
+
+match_data('./parents.json', './childrens.json')
+console.log("Notification : Data sedang diproses !");
