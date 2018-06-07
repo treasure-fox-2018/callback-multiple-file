f9c6ddf4208be0c59b799e9f8c0b0ba9e1bd700d
fajartc02 <fajartc02@gmail.com>
2018-06-08 01:29:33 +0700

    release 0

--- index.js
+++ index.js
@@ -1,9 +1,27 @@
 const fs = require('fs');
-var sleep = require('sleep');
+// var sleep = require('sleep');
+
+
+function sleep(milliseconds) {
+  var start = new Date().getTime();
+  for (var i = 0; i < 1e7; i++) {
+    if ((new Date().getTime() - start) > milliseconds) {
+      break;
+    }
+  }
+}
 
 function match_data(parent_file, children_file) {
   // your code here...
+  const parrent = require('./parents.json') // convert json to array of object
+  const childrens = require('./childrens.json')
+  let mainArr = []
+  for (let i = 0; i < parrent.length; i++) {
+    console.log(parrent[i])
+    sleep(5000)
+  }
 }
 
 match_data('./parents.json', './childrens.json')
 console.log("Notification : Data sedang diproses !");
+console.log(match_data)
