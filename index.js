eb50380fdf0a18537b91ae15f91afae6b7830248
Ari Supriatna <arisupriatna@fabio-Aspire-E5-476G.localdomain>
2018-06-08 11:43:17 +0700

    challenge done

--- index.js
+++ index.js
@@ -1,8 +1,41 @@
 const fs = require('fs');
 var sleep = require('sleep');
 
+function sleep(milliseconds) {
+  var start = new Date().getTime();
+  for (var i = 0; i < 1e7; i++) {
+    if ((new Date().getTime() - start) > milliseconds) {
+      break;
+    }
+  }
+}
+
 function match_data(parent_file, children_file) {
-  // your code here...
+  fs.readFile(parent_file, 'utf8', function (err, parent_file) {
+    if (err) throw err
+
+    const parentFile = JSON.parse(parent_file)
+    sleep.sleep(5)
+
+    fs.readFile(children_file, 'utf8', function (err, children_file) {
+      if (err) throw err
+
+      const childrenFile = JSON.parse(children_file)
+      sleep.sleep(5)
+      for (let i = 0; i < parentFile.length; i++) {
+        var childParent = []
+        for (let j = 0; j < childrenFile.length; j++) {
+          if (parentFile[i].last_name === childrenFile[j].family) {
+            childParent.push(childrenFile[j].full_name)
+          }
+        }
+        parentFile[i].children = childParent
+        console.log(parentFile)
+      }
+    })
+    
+  })
+  
 }
 
 match_data('./parents.json', './childrens.json')
