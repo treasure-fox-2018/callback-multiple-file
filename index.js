4eefccd33a98393c91700e10d2382465d9bbce1c
lockonmaram <maramstratos@gmail.com>
2018-06-08 08:46:59 +0700

    callback-multiple-file done

--- index.js
+++ index.js
@@ -1,9 +1,31 @@
 const fs = require('fs');
-var sleep = require('sleep');
+const parent_file = JSON.parse(fs.readFileSync('parents.json'));
+const children_file = JSON.parse(fs.readFileSync('childrens.json'));
 
-function match_data(parent_file, children_file) {
-  // your code here...
+function sleep(milliseconds) {
+    var start = new Date().getTime();
+    for (var i = 0; i < 1e7; i++) {
+      if ((new Date().getTime() - start) > milliseconds) {
+        break;
+      }
+    }
+  }
+
+function match_data(parent_file, children_file, callback) {
+  for (let i = 0; i < parent_file.length; i++) {
+    var childrenArr = [];
+    for (let j = 0; j < children_file.length; j++) {
+      if (parent_file[i].last_name === children_file[j].family) {
+        childrenArr.push(children_file[j].full_name);
+      }
+    }
+    parent_file[i].children = childrenArr;
+    sleep(500);
+    callback(parent_file)
+  }
 }
 
-match_data('./parents.json', './childrens.json')
-console.log("Notification : Data sedang diproses !");
+match_data(parent_file, children_file, function(parent_file){
+  console.log(parent_file);
+  console.log("Notification : Data sedang diproses !");
+})
