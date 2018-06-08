79f20e88429af9f6c7cdebe80a21765f1e45a492
HelmiYogantara <helmiyogantara@gmail.com>
2018-06-08 10:26:16 +0700

    pake fs.readfile

--- index.js
+++ index.js
@@ -14,27 +14,47 @@ function sleep(milliseconds) {
 
 function match_data(parent_file, children_file) {
   // your code here...
-  fs.readFile(parent_file,(err,data))
-  var importParents = fs.readFileSync(parent_file,'utf8')
-  var parent_data = JSON.parse(importParents)
-
-  var importChil = fs.readFileSync(children_file,'utf8')
-  var child_data = JSON.parse(importChil)
-
-  for(var i = 0; i < parent_data.length; i++){
-    var arrChildren = []
-    for(var j = 0; j < child_data.length; j++){
-      if(parent_data[i].last_name === child_data[j].family){
-        arrChildren.push(child_data[j].full_name)
+  fs.readFile(parent_file,(err,data_parent) => {
+    if (err) throw err;
+    // console.log(data);
+    var parentArrObj = JSON.parse(data_parent)
+    fs.readFile(children_file,(err, data_child) =>{
+      if(err) throw err;
+      var childArrObj = JSON.parse(data_child)
+      for(var i = 0; i < parentArrObj.length; i++){
+        var arrChildren = []
+        for(var j = 0; j < childArrObj.length; j++){
+          if(parentArrObj[i].last_name === childArrObj[j].family){
+            arrChildren.push(childArrObj[j].full_name)
+          }
+          parentArrObj[i].childre = arrChildren
+        }
+        sleep(1000)
+        console.log(parentArrObj);
+        
       }
-      parent_data[i].children = arrChildren
-    }
-  }
-  console.log(parent_data);
-  for(var i = 0; i < parent_data.length; i++){
-    console.log(parent_data[i]);
-    sleep(5000)
-  }
+    })
+  })
+  // var importParents = fs.readFileSync(parent_file,'utf8')
+  // var parent_data = JSON.parse(importParents)
+
+  // var importChil = fs.readFileSync(children_file,'utf8')
+  // var child_data = JSON.parse(importChil)
+
+  // for(var i = 0; i < parent_data.length; i++){
+  //   var arrChildren = []
+  //   for(var j = 0; j < child_data.length; j++){
+  //     if(parent_data[i].last_name === child_data[j].family){
+  //       arrChildren.push(child_data[j].full_name)
+  //     }
+  //     parent_data[i].children = arrChildren
+  //   }
+  // }
+  // console.log(parent_data);
+  // for(var i = 0; i < parent_data.length; i++){
+  //   console.log(parent_data[i]);
+  //   sleep(5000)
+  
   
 }
 
