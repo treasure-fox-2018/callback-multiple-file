817e45a072c9dde783abdf819a6465afe6d649a0
Hacktiv8 <hacktiv8@H8JKT002.localdomain>
2018-06-08 11:45:21 +0700

    initial commit

--- index.js
+++ index.js
@@ -1,9 +1,36 @@
 const fs = require('fs');
-var sleep = require('sleep');
+const sleep = require('sleep');
 
 function match_data(parent_file, children_file) {
-  // your code here...
+  console.log('Notification: Data sedang diproses')
+  fs.readFile(parent_file, (err, data) => {
+    sleep.sleep(1);
+    if (err) throw err;
+    let parentData = JSON.parse(data);
+
+    fs.readFile(children_file, (err, data) => {
+      sleep.sleep(1);
+      if (err) throw err;
+      let childData = JSON.parse(data);
+
+
+    });
+
+  });
+
+
 }
 
+// function readData(path, callback) {
+//   fs.readFile(path, (err, data) => {
+//     if (err) throw err;
+//     callback(JSON.parse(data));
+//   });
+// }
+
+
+
+
+
 match_data('./parents.json', './childrens.json')
-console.log("Notification : Data sedang diproses !");
+// console.log("Notification : Data sedang diproses !");
