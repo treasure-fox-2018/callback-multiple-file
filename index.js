4a814b8b7f0c1e29888f5833bd406d90649c0dc3
fajartc02 <fajartc02@gmail.com>
2018-06-08 13:47:31 +0700

    completed callbackMultiple

--- index.js
+++ index.js
@@ -11,17 +11,41 @@ function sleep(milliseconds) {
   }
 }
 
-function match_data(parent_file, children_file) {
+function match_data(parent_file, children_file, callback) {
   // your code here...
-  const parrent = require('./parents.json') // convert json to array of object
-  const childrens = require('./childrens.json')
-  let mainArr = []
-  for (let i = 0; i < parrent.length; i++) {
-    console.log(parrent[i])
-    sleep(5000)
-  }
+  // for (let i = 0; i < parrent.length; i++) {
+  //   parrent[i].children = []
+  //   console.log(parrent[i])
+  //   sleep(5000) // 5sec
+  // }
+  fs.readFile(parent_file, function(err, dataParent){
+    if (err) throw err
+    let parentData = JSON.parse(dataParent)
+    sleep(1000)
+    // callback(parentData);
+    fs.readFile(children_file, function(err, dataChildren){
+      if(err) throw err
+      let childrenData = JSON.parse(dataChildren)
+      sleep(1000)
+      // callback(childrenData);
+      for(let i = 0; i < parentData.length; i++) {
+        parentData[i].children = []
+        for(let j = 0; j < childrenData.length; j++) {
+          if(parentData[i].last_name === childrenData[j].family) {
+            parentData[i].children.push(childrenData[j].full_name)
+          }
+        }
+        callback(parentData);
+        
+      }
+      // callback(parentData)
+    })
+  })
 }
 
-match_data('./parents.json', './childrens.json')
 console.log("Notification : Data sedang diproses !");
-console.log(match_data)
+match_data('./parents.json', './childrens.json', function(data){
+  // console.log(match_data)
+  console.log(data);
+  
+})
