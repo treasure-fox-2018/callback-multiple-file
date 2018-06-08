0dbd2b25778196277ac2f2b523448c68041241b4
dawimuhammad <muhamadhaddawi@gmail.com>
2018-06-08 10:47:23 +0700

    Release 0 - Callback read multiple file DONE

--- index.js
+++ index.js
@@ -1,9 +1,41 @@
 const fs = require('fs');
 var sleep = require('sleep');
 
-function match_data(parent_file, children_file) {
-  // your code here...
+let parent_file = './parents.json'
+let children_file = './childrens.json'
+
+function match_data() {
+    read_file(parent_file, function(parentData) {
+        read_file(children_file, function(childData) {
+            // loop parent data
+            for (let i = 0; i < parentData.length; i++) {
+                let parent = parentData[i]
+                let childs = []
+                // loop childrens with same family name
+                for (let j = 0; j < childData.length; j++) {
+                    let child = childData[j]
+                    let isChildFamily = parent.last_name === child.family
+
+                    if (isChildFamily) {
+                        childs.push(child.full_name)
+                    }
+
+                    parent.children = childs
+                }
+            }
+
+            console.log(parentData)
+        })
+    })
 }
 
-match_data('./parents.json', './childrens.json')
+function read_file(fileName, callback) {
+    let fileData = fs.readFile(fileName, 'utf8', function(err, data) {
+      if (err) throw err
+          callback(JSON.parse(data))
+          sleep.sleep(5)
+    })
+}
+
+match_data()
 console.log("Notification : Data sedang diproses !");
