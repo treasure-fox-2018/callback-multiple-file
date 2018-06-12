69b0c53d6a31097ad6bd7da0888dfbdbd05cf1cc
Ari Supriatna <arisupriatna@MacBook-Air-Ari.local>
2018-06-12 12:15:17 +0700

    fix callback-multiple

--- index.js
+++ index.js
@@ -1,27 +1,19 @@
 const fs = require('fs');
 var sleep = require('sleep');
 
-function sleep(milliseconds) {
-  var start = new Date().getTime();
-  for (var i = 0; i < 1e7; i++) {
-    if ((new Date().getTime() - start) > milliseconds) {
-      break;
-    }
-  }
-}
-
 function match_data(parent_file, children_file) {
-  fs.readFile(parent_file, 'utf8', function (err, parent_file) {
+  fs.readFile(parent_file, 'utf8', (err, parent_file) => {
     if (err) throw err
 
     const parentFile = JSON.parse(parent_file)
     sleep.sleep(5)
 
-    fs.readFile(children_file, 'utf8', function (err, children_file) {
+    fs.readFile(children_file, 'utf8', (err, children_file) => {
       if (err) throw err
 
       const childrenFile = JSON.parse(children_file)
       sleep.sleep(5)
+
       for (let i = 0; i < parentFile.length; i++) {
         var childParent = []
         for (let j = 0; j < childrenFile.length; j++) {
@@ -30,8 +22,8 @@ function match_data(parent_file, children_file) {
           }
         }
         parentFile[i].children = childParent
-        console.log(parentFile)
       }
+      console.log(parentFile)
     })
     
   })
